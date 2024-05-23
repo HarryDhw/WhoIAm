b2d908a1ad37d7b6a8a1990269eec369b9333245
HarryDhw <greatd24@icloud.com>
2024-05-23 16:51:43 +0800

    judge 1

--- baccarat.py
+++ baccarat.py
@@ -2,7 +2,9 @@ import random
 
 class Baccarat:
     def __init__(self) -> None:
-        pass
+        self.results = []
+        self.usedcards = []
+        self.record = []
 
     def makecards(self):
         suits = ['Spade', 'Heart', 'Club', 'Diamond']
@@ -26,30 +28,77 @@ class Baccarat:
         removed = [card]
         for _ in range(n):
             removed.append(self.cards.pop())
-        print(removed)
+        self.usedcards.extend(removed)
+        print(self.usedcards)
         print('Removed:%s, Rest:%s' % (n + 1, len(self.cards)))
 
     def play(self):
-        player = {'cards':[], 'amount':0, 'win':False}
-        banker = {'cards':[], 'amount':0, 'win':False}
-        a, b, c, d = [self.cards.pop() for _ in range(4)]
+        player = {'cards':[], 'amount':0, 'win':False, 'double':False}
+        banker = {'cards':[], 'amount':0, 'win':False, 'double':False, 'issixwin':False}
+        removed = [self.cards.pop() for _ in range(4)]
+        a, b, c, d = removed
         player['cards'] = [a, c]
         banker['cards'] = [b, d]
+        player['double'] = a[0] == c[0]
+        banker['double'] = b[0] == d[0]
+
         pn, bn = 0, 0
         for card in player['cards']:
-            try:
-                pn += card[0]
-            except:
-                pn += 0
+            pn += self.addnum(card)
+            pn = pn % 10
         for card in banker['cards']:
-            try:
-                bn += card[0]
-            except:
-                bn += 0
-        player['amount'] = pn % 10
-        banker['amount'] = bn % 10
+            bn += self.addnum(card)
+            bn = bn % 10
+
+        if (pn >= 8 or bn >= 8) or (pn >= 6 and bn >= 6):
+            self.judge(pn, bn, player, banker)
+        else:
+            if pn >= 6:
+                bn, banker = self.bankerAddCard(bn, banker, removed)
+                self.judge(pn, bn, player, banker)
+
+        self.usedcards.extend(removed)
+        self.record.append((player, banker))
         print(player, banker, sep='\n')
-        print('Removed:%s, Rest:%s' % (4, len(self.cards)))
+        print('Removed:%s, Rest:%s' % (len(removed), len(self.cards)))
+        print(self.results)
+        print(self.record)
+
+    def addnum(self, card):
+        try:
+            n = card[0] / 1
+            return n
+        except:
+            return 0
+        
+    def judge(self, pn, bn, player, banker):
+        player['amount'] = pn
+        banker['amount'] = bn
+        if pn > bn:
+            self.results.append('P')
+            player['win'] = True
+        elif pn < bn:
+            self.results.append('B')
+            banker['win'] = True
+        else:
+            self.results.append('T')
+
+    def playerAddCard(self, pn, player, removed):
+        card = self.cards.pop()
+        removed.append(card)
+        player['cards'].append(card)
+        pn += self.addnum(card)
+        pn %= 10
+        return pn, player
+    
+    def bankerAddCard(self, bn, banker, removed):
+        card = self.cards.pop()
+        removed.append(card)
+        banker['cards'].append(card)
+        bn += self.addnum(card)
+        bn %= 10
+        return bn, banker
+
 
 
 if __name__ == '__main__':
